4cb8e590e90ee74bd542f0f1720e88dc775f4ac2
Fernando <cuzcator@gmail.com>
2019-08-16 19:36:11 -0500

    Creando la tabla para libros

--- frontend/src/components/Libro/ListaLibro.vue
+++ frontend/src/components/Libro/ListaLibro.vue
@@ -3,6 +3,13 @@
     <div class="row">
       <div class="col text-left">
         <h2>Lista de libros</h2>
+        <div class="col-md-12">
+          <b-table striped hover :items="libros" :fields="fields">
+
+          </b-table>
+        </div>
+
+        </div>
       </div>
     </div>
   </div>
@@ -10,7 +17,16 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      fields: [
+        {key: 'title', label: 'Título'},
+        {key: 'description', label: 'Descripción'},
+        {key: 'action', label: 'Acción'}
+      ],
+      libros: []
+    }
+  }
 }
 </script>
 <style lang="css" scoped>
